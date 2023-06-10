e01e4415d3293c95344da9dfe7fcb3f8e7f26230
Ezequiel Perez <106891582+cheque17@users.noreply.github.com>
2023-06-09 18:31:42 -0600

    Edit index.html, styles.css and script.js

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
+
 const gameBoardModule = (() => {
   const tiles = document.querySelectorAll('.grid.item');
   const gameBoard = Array.from(tiles);
-  return { gameBoard: gameBoard }
+
+  const showGame = () => {
+    tiles.forEach(() => {
+      tiles.addEventListener('click', displayController.interaction)
+    })
+  }
+  return {gameBoard}
 })();
 
-const displayController = (() => {
-  return{};
-})();
-
-const playerCreator = (number, marker) => {
-  return {number, marker};
+const playerCreator = (name, marker) => {
+  return {name, marker};
 }
 
-const player1 = playerCreator(1, 'o');
-const player2 = playerCreator(2, 'x');
+const displayController = (() => {
+  let players = [];
+  let playerTurn;
+  let gameOver = false;
+
+  let player1Name = document.querySelector('#player1').value;
+  let player2Name = document.querySelector('#player2').value;
+
+  const startGame = () => {
+    players = [
+      playerCreator(player1Name, 'X'),
+      playerCreator(player2Name, 'O')
+    ]
+    playerTurn = 0;
+    gameOver = false;
+  }
+
+  return{
+    startGame,
+  };
+})();
+
+
+
+
+//Event Listener
+
+const startGameButton = document.querySelector('#start');
+startGameButton.addEventListener('click', () => {
+  displayController.startGame();
+  console.log('Button working');
+})
+
